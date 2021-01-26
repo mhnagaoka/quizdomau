6b09fcdfef2385a7ffbc24efb24c677d527813c7
Mauricio H Nagaoka <mauricio.nagaoka@visto.global>
2021-01-25 21:37:57 -0300

    Adiciona logo

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import styled from "styled-components";
 import db from "../db.json";
 import Widget from "../src/components/Widget";
+import QuizLogo from "../src/components/QuizLogo";
 import QuizBackground from "../src/components/QuizBackground";
 import Footer from "../src/components/Footer";
 import GitHubCorner from "../src/components/GitHubCorner";
@@ -20,6 +21,7 @@ export default function Home() {
   return (
     <QuizBackground backgroundImage={db.bg}>
       <QuizContainer>
+        <QuizLogo />
         <Widget>
           <Widget.Header>
             <h1>{db.title}</h1>
