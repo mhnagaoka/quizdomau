f110ca6e9ef9c2848a2c7ff63c94a5aa143c9311
Mauricio H Nagaoka <mauricio.nagaoka@visto.global>
2021-01-27 23:54:29 -0300

    Ajustes de estilo, roteamento p/ página de quiz

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styled from 'styled-components'
+import { useRouter } from 'next/router'
+
 import db from '../db.json'
 import Widget from '../src/components/Widget'
 import QuizLogo from '../src/components/QuizLogo'
@@ -18,7 +20,40 @@ const QuizContainer = styled.div`
   }
 `
 
+const PlayerInput = styled.input`
+  width: 100%;
+  height: 3em;
+  background-color: ${({colors}) => colors.mainBg};
+  border-radius: 3.5px;
+  border-width: 1px;
+  border-style: solid;
+  border-color: ${({colors}) => colors.primary};
+  padding:  8px 16px;
+  color: ${({colors}) => colors.contrastText};
+  &:focus {
+    outline: none;
+  }
+`
+
+const PlayerButton = styled.button`
+  width: 100%;
+  height: 3em;
+  margin-top: 25px;
+  text-transform: uppercase;
+  font-family: Lato;
+  background-color: ${({colors}) => colors.secondary};
+  color: ${({colors}) => colors.contrastText};
+  border-radius: 4px;
+  border: none;
+  &:focus {
+    outline: none;
+  }
+`
+
 export default function Home() {
+  const router = useRouter()
+  const [ name, setName ] = useState('')
+
   return (
     <>
       <QuizBackground backgroundImage={db.bg}>
@@ -30,6 +65,23 @@ export default function Home() {
             </Widget.Header>
             <Widget.Content>
               <p>{db.description}</p>
+              <form
+                onSubmit={(evt) => {
+                  evt.preventDefault()
+                  router.push(`/quiz?name=${name}`)
+                }}
+              >
+                <PlayerInput
+                  onChange={(evt) => {
+                    setName(evt.target.value)
+                  }}
+                  placeholder="Seu nome"
+                  colors={db.theme.colors}
+                />
+                <PlayerButton type="submit" disabled={name.length === 0} colors={db.theme.colors}>
+                  Jogar
+                </PlayerButton>
+              </form>
             </Widget.Content>
           </Widget>
           <Footer />
