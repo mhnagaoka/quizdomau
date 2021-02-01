a43ebddb46c5eaa455b152c94427205850bce87c
Mauricio H Nagaoka <mauricio.nagaoka@visto.global>
2021-01-31 22:00:11 -0300

    Pequeno refactoring

--- pages/quiz.js
+++ pages/quiz.js
@@ -1,4 +1,5 @@
 import { useRouter } from 'next/router'
+import PropTypes from 'prop-types'
 import React from 'react'
 import styled from 'styled-components'
 import db from '../db.json'
@@ -150,12 +151,61 @@ function LoadingWidget() {
   )
 }
 
+function QuestionWidget({
+  questionNumber,
+  totalQuestions,
+  question,
+  onConfirm,
+}) {
+  const questionId = `question__${questionNumber}`
+  return (
+    <Widget>
+      <Widget.Header>
+        <h2>
+          Pergunta {questionNumber} de {totalQuestions}
+        </h2>
+      </Widget.Header>
+      <Widget.Content>
+        <QuestionImage src={question.image} alt={question.title} />
+        <h3>{question.title}</h3>
+        <p>{question.description}</p>
+        <form>
+          {question.alternatives.map((a, i) => {
+            const alternativeId = `alternative__${i}`
+            return (
+              <Widget.Topic as="label" htmlFor={alternativeId}>
+                <input id={alternativeId} type="radio" name={questionId} />
+                {a}
+              </Widget.Topic>
+            )
+          })}
+        </form>
+        <ConfirmButton onClick={onConfirm} colors={db.theme.colors}>
+          Confirmar
+        </ConfirmButton>
+      </Widget.Content>
+    </Widget>
+  )
+}
+
+QuestionWidget.propTypes = {
+  questionNumber: PropTypes.number.isRequired,
+  totalQuestions: PropTypes.number.isRequired,
+  question: PropTypes.shape({
+    image: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    description: PropTypes.string.isRequired,
+    alternatives: PropTypes.arrayOf(PropTypes.string).isRequired,
+  }).isRequired,
+  onConfirm: PropTypes.func.isRequired,
+}
+
 export default function QuizPage() {
   const router = useRouter()
   const { name, q = 1 } = router.query
-  const qNum = parseInt(q, 10)
-  const question = db.questions[qNum - 1]
-  const count = db.questions.length
+  const questionNumber = parseInt(q, 10)
+  const question = db.questions[questionNumber - 1]
+  const totalQuestions = db.questions.length
 
   // Isso parece q não funciona direito. Dá pau quando recarrega a página.
   //   useEffect(() => {
@@ -171,37 +221,14 @@ export default function QuizPage() {
         <QuizContainer>
           <QuizLogo />
           <LoadingWidget />
-          <Widget>
-            <Widget.Header>
-              <h2>
-                Pergunta {qNum} de {count}
-              </h2>
-            </Widget.Header>
-            <Widget.Content>
-              <QuestionImage src={question.image} alt={question.title} />
-              <h3>{question.title}</h3>
-              <p>{question.description}</p>
-              <ul>
-                {question.alternatives.map((a) => {
-                  return (
-                    <li>
-                      <AlternativeButton colors={db.theme.colors} type="button">
-                        {a}
-                      </AlternativeButton>
-                    </li>
-                  )
-                })}
-              </ul>
-              <ConfirmButton
-                onClick={() => {
-                  router.push(`/quiz?name=${name}&q=${qNum + 1}`)
-                }}
-                colors={db.theme.colors}
-              >
-                Confirmar
-              </ConfirmButton>
-            </Widget.Content>
-          </Widget>
+          <QuestionWidget
+            questionNumber={questionNumber}
+            totalQuestions={totalQuestions}
+            question={question}
+            onConfirm={() => {
+              router.push(`/quiz?name=${name}&q=${questionNumber + 1}`)
+            }}
+          />
           <Footer />
           <GitHubCorner projectUrl="https://github.com/mhnagaoka/quizdomau" />
         </QuizContainer>
